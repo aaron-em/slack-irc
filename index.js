45238d74ee6ec96b37367e428fe6271abb5c168c
Aaron Miller <me@aaron-miller.me>
2014-12-09 13:21:51 -0500

    add http listener w/hello world

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var config = require('config')
+  , express = require('express')
   , credentials = require('./credential')
   , IRC = require('./lib/irc')
   , Slack = require('./lib/slack')
   , ircClient
-  , slackClient;
+  , slackClient
+  , httpListener;
 
 slackClient = new Slack(config, credentials.slack);
 ircClient = new IRC(config);
+httpListener = express();
 
 ircClient.on('message', function(data) {
   slackClient.send(data.channel.slack, data.from, data.message);
@@ -14,4 +17,13 @@ ircClient.on('message', function(data) {
 
 ircClient.connect(function() {
   console.log('Connected to IRC as ' + config.irc.nick + '.');
+});
+
+httpListener.get('/', function(req, res) {
+  res.status(200)
+    .send('Hi there.');
+});
+
+httpListener.listen(80, function() {
+  console.log('Listening for HTTP connections on port 80.');
 });
