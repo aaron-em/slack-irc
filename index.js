0a26dcf1bece63cf1f2465e575dc3c921afecedb
Aaron Miller <me@aaron-miller.me>
2014-12-09 09:16:51 -0500

    modularize slack & irc clients, general cleanup

--- index.js
+++ index.js
@@ -1,57 +1,17 @@
 var config = require('config')
   , credentials = require('./credential')
-  , request = require('request')
-  , irc = require('irc')
-  , channels = []
-  , client;
+  , IRC = require('./lib/irc')
+  , Slack = require('./lib/slack')
+  , ircClient
+  , slackClient;
 
-function ircToSlack(channel) {
-  return function(from, message, raw) {
-    request.post(
-      {
-        uri: config.slack.webhook.uri + credentials.slack,
-        body: JSON.stringify({
-          'channel': channel.slack,
-          'username': from + ' via IRC',
-          'icon_emoji': ':speech_balloon:',
-          'text': message
-        })
-      },
-      function(err, res) {
-        if (err) console.log(err.message);
-      }
-    );
-  };
-}
+slackClient = new Slack(config, credentials.slack);
+ircClient = new IRC(config);
 
-function slackToIrc() {
-  throw new Error('EUNIMPL');
-}
-
-function addChannelListeners() {
-  config.channels.forEach(function(channel) {
-    client.on('message' + channel.irc, ircToSlack(channel));
-  });
-};
-
-client = new irc.Client(config.irc.host, config.irc.nick, {
-  autoConnect: false,
-  userName: config.irc.nick,
-  port: config.irc.port || 6667,
-  channels: config.channels.map(function(c) { return c.irc; }),
-  debug: true
+ircClient.on('message', function(data) {
+  slackClient.send(data.channel.slack, data.from, data.message);
 });
 
-client.on('registered', function(msg) {
-  console.log('Registered');
-  console.log(msg);
-});
-
-client.on('error', function(err) {
-  console.log(err);
-});
-
-client.connect(function() {
-  console.log('Connected!');
-  addChannelListeners();
+ircClient.connect(function() {
+  console.log('Connected to IRC as ' + config.irc.nick + '.');
 });
